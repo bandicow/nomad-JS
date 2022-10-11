ecab506997ed01048972496af41fca5511dc445e
JUN HYEOK KIM <kjh45388117@gmail.com>
2022-10-11 23:35:24 +0900

    1011

---
+++ JS_Desk/pt.js
@@ -0,0 +1,46 @@
+// const NAME= "Mike"; //문자열 String
+
+// const NAME1="Mik";
+// const NAME2='Mak';
+// const NAME3=`Muk`; 
+
+// const MESSAGE = "I'm a boy";
+// const MESSAGE2 = 'I\'m a boy'; // 같은 ' 사용시 역슬래시 \를 사용
+// const MESSAGE3 = `I'm a ${NAME}`; // 문자열 내부에 변수 선언할 때 사용
+// const MESSAGE4 = `I'm ${27-1}살`
+
+// console.log(MESSAGE4);
+
+// const AGE = 30; //숫자형
+// const PI = 3.14;
+
+// console.log(1+2) // + 더하기
+// console.log(1-2) // - 빼기
+// console.log(1*2) // * 곱하기
+// console.log(1/2) // / 나누기
+// console.log(1%2) // % 나머지
+
+// const x = 1/0; //무한대 Infinity로 뜸
+// const y = NAME/2; //NaN로 뜸
+// console.log(x);
+// console.log(y);
+
+// const a = true; // 참
+// const b = false; // 거짓
+
+// console.log(NAME=='Mike') //참
+// console.log(AGE < 0) //거짓
+
+// let user = null;
+// console.log (user) //null
+
+// let age;
+// console.log(age) // undefined
+
+
+// console.log(typeof 3);
+// console.log(typeof NAME);
+// console.log(typeof true);
+// console.log(typeof "xxx");
+// console.log(typeof null);
+// console.log(typeof undefined);
--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 document.title = "Doragi";
+
+const h1 = document.querySelector("div.hello h1");
+
+console.log(h1);
+
+// JS 가 HTML을 건드리면 HTML을 바라보는 CSS가 변경된 HTML에 맞춘다.
+// h1.className는 getter이면서 setter
+function handleh1click() {
+    const clickedClass = "clicked";
+    if (h1.className===clickedClass) {
+        h1.className = "";
+    }
+    else {
+        h1.className = clickedClass;
+    }
+};
+
+
+
+h1.addEventListener("click",handleh1click);
