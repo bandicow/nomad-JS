3443fe9d8fe2115c22246f46cc34e870cf56a4b1
JUN HYEOK KIM <kjh45388117@gmail.com>
2022-10-13 21:26:19 +0900

    tes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 document.title = "Doragi";
 
-<<<<<<< HEAD
 // 1ë²ˆì§¸
 // const loginForm = document.querySelector("#login-form");
 // const loginInput = loginForm.querySelector("input");
@@ -14,12 +13,10 @@ function onLoginBtnClick() {
     const username = loginInput.value;
     console.log(username)
     }
-};
-
 
 loginButton.addEventListener('click',onLoginBtnClick);
-=======
-const h1 = document.querySelector('.hello:first-child h1');
+
+const h1 = document.querySelector('.hello h1');
 
 console.dir(h1);
 
@@ -27,11 +24,11 @@ function handleTitleclick(){
     console.log("title click!");
     h1.style.color = "blue";
 }
+
 function handlemousewheel(){
     console.log("title wheel!");
     h1.style.color = "black";
 }
-
 function handlemouseenter(){
     console.log("mouse is here!");
     h1.innerText = "mouse is here!";
@@ -42,7 +39,7 @@ function handlemouseleave(){
     h1.innerText = "mouse is gone..";
 }
 
-// hello.onclick = handleTitleclick;
+h1.onclick = handleTitleclick;
 h1.addEventListener('click',handleTitleclick);
 
 h1.addEventListener('mousewheel',handlemousewheel);
@@ -65,11 +62,3 @@ function handleWindowonline() {
     alert("online!");
 }
 
-//Ã¢ Å©±â º¯ÇÏ¸é ¹Ù·Î »¡°³Áü
-window.addEventListener("resize",handleWindowResize);
-window.addEventListener("copy",handleWindowcopy);
-
-// wifi Åë½Å ¿©ºÎ
-window.addEventListener("offline",handleWindowoffline);
-window.addEventListener("online",handleWindowonline);
->>>>>>> 7d8c9122be3f5c80cf86514a5d9889cb8f26a08e
